211a40926f9780bd2e6c153a9e90fa833d210e1e
Loki <95887189+B1Fr0st@users.noreply.github.com>
2022-05-02 12:14:42 -0700

    Add run checks

--- dedupe.py
+++ dedupe.py
@@ -1,1 +1,16 @@
+#dedupe.py
+import hashlib as hash
+import argparse
 
+#Tries to import win10toast if installed, if not no notifications show.
+try:
+  import win10toast
+  notify = True
+except ImportError:
+  noNotify = False
+  
+#Checks whether or not it was run from the CLI
+if sys.argv:
+  commandLine = True
+else:
+  commandline = False
